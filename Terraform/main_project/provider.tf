627f367b59fe1bc6db618a19f4d5c87268d2f96f
ramyasanthu <ramyassramya@gmail.com>
2023-09-28 15:40:13 +0530

    add

--- Terraform/main_project/provider.tf
+++ Terraform/main_project/provider.tf
@@ -1,5 +1,5 @@
-provider "aws" {
-    access_key = ""
-    secret_key = ""
-    region = ""
+provider "aws"  {
+    access_key = "AKIAVGE7A74FXL3VPBKR"
+    secret_key = "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"
+    region = "us-west-2"
 }
